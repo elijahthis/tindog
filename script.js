d0e3e6176fe0e9d0747f840f09dde8d0497e1f53
elijahthis <emperorelijah007@gmail.com>
2022-02-23 05:06:33 +0100

    static site

---
+++ script.js
@@ -0,0 +1,15 @@
+const quotes = [
+  {
+    quote:
+      "I no longer have to sniff other dogs for love. I've found the hottest Corgi on TinDog. Woof!",
+    name: "Pebbles",
+    locale: "New York",
+  },
+  {
+    quote:
+      "My dog used to be so lonely, but with TinDog's help, they've found the love of their life. I think.",
+    name: "Beverly",
+    locale: "Illinois",
+  },
+];
+
